679091e7a8a50e429db316a3525596225f9463eb
stephenbrown1990 <stephen@asdsolutions.co.uk>
2014-09-21 11:43:46 +0100

    update

--- giverandom.js
+++ giverandom.js
@@ -420,5 +420,5 @@ function isNumber(n) {
 }
 
 app.listen(app.get('port'), function () {
-    console.log("Node app is running at localhost:" + app.get('port'))
+    console.log("Node app is running at localhost:" + app.get('port'));
 });
