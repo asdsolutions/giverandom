d044fbd850b49592bfeb946023409f0b8a946bd8
stephenbrown1990 <stephen@asdsolutions.co.uk>
2014-09-21 11:49:05 +0100

    hello

--- giverandom.js
+++ giverandom.js
@@ -47,10 +47,7 @@ var mongoUri = process.env.MONGOLAB_URI ||
         'mongodb://localhost/mydb';
 
 mongo.Db.connect(mongoUri, function (err, db) {
-    db.collection('mydocs', function (er, collection) {
-        collection.insert({'mykey': 'myvalue'}, {safe: true}, function (er, rs) {
-        });
-    });
+    
 });
 
 // Mongo Database
